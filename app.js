72655fdcf147c835b3b95c9a84eca0f61c976c7f
alessandrob17 <a.baldipardi@gmail.com>
2023-02-07 18:11:35 +0100

    FEAT while + if +nan + dispari

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ let numInseriti = 0;
 
 while (numDispari.length < 10) {
   let num = parseInt(prompt("Inserisci un numero:"));
-  
+  if (isNaN(num)) {
+    continue;
+  }
+
+  if (num % 2 !== 0) {
+    numDispari.push(num);
+  }
   
 }
 
